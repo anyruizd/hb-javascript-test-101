21d4eb7d027798e3e4b09abb2c181c8fac52e48f
anyruizd <anyruizd@gmail.com>
2017-11-14 10:58:51 -0500

    Add second function and functionalfunction

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 function generateRandomNumber(){
-    let color = {};
+    const color = {};
     color.r = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
     color.g = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
     color.b = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
     return color;
 }
 
+
+function filterNegativeNumbers(array){
+    let newArray = [];
+    for(let i = 0; i < array.length; i++){
+        if (array[i] >= 0){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+
+
+function functionalfilterNegativeNumbers(array){
+    return newArray = array.filter(function(x){ return x >= 0 });
+}
+
 // First function test
 console.log(generateRandomNumber());
+
+// Second function test 
+console.log(filterNegativeNumbers([-4, -6, -5, 0, 34, 54, -7]));
+
+// Secon function (functional) test
+console.log(functionalfilterNegativeNumbers([-4, -6, -5, 0, 34, 54, -7]));
