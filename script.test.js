bafe4dae563412387f429a2ff429de65c07d9ac7
anyruizd <anyruizd@gmail.com>
2017-11-15 13:00:43 -0500

    Adding first tests

--- script.test.js
+++ script.test.js
@@ -1,9 +1,28 @@
 import {
-    generateRandomcolor
+    generateRandomColor,
+    filterNegativeNumbers,
+    unctionalFilterNegativeNumbers,
+    mapNumbersIntoStrings,
+    functionalMapNumbersIntoStrings,
+    printType,
+    isPalindrome,
+    Person,
+    printOutPersonAge
 } from './script.js';
 
 describe ('generateRandomColor', () =>{
-    it('should work', () => {
-        expect(1).toBe(1);
+    it('should return an object containing r: g: b: ', () =>{
+        expect(generateRandomColor()).toMatchObject({
+            r: expect.any(Number),
+            g: expect.any(Number),
+            b: expect.any(Number)
+        });
     });
 });
+
+describe('filterNegativeNumbers', () =>{
+    it('should return a new array with positive numbers only', () => {
+        expect(filterNegativeNumbers([-4, -6, -5, 0, 34, 54, -7])).toEqual([0, 34, 54]);
+    });
+});
+
